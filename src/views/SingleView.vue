c326d2197832b01bab4a17fbddd5f40058df29a1
Mjakaba13 <malcolmakaba@gmail.com>
2022-07-18 15:12:47 +0000

    Update

--- src/views/SingleView.vue
+++ src/views/SingleView.vue
@@ -7,21 +7,21 @@
                 <div class="back">
                     <div class="same">
                         <img src='@/assets/bxs_category.svg' alt="">
-                        <p>CATEGORY</p>
+                        <p class="tabs">CATEGORY</p>
                     </div>
                     <h4>{{ single.strCategory }}</h4>
                 </div>
                 <div class="back">
                     <div class="same">
                         <img src='@/assets/ci_location.svg' alt="">
-                        <p>AREA</p>
+                        <p class="tabs">AREA</p>
                     </div>
                     <h4>{{ single.strArea }}</h4>
                 </div>
                 <div class="back">
                     <div class="same">
                         <img src='@/assets/fa6-solid_tags.svg' alt="">
-                        <p>TAGS</p>
+                        <p class="tabs">TAGS</p>
                     </div>
                     <h6>{{ single.strTags }}</h6>
                 </div>
@@ -33,13 +33,14 @@
     <section class="bottom">
         <div class="ingredients">
             <h1>Ingredients.</h1>
-            <div class="ingre">
-                <div>
-                    <span>{{ single.strIngredient1 }}</span>
-                    <span>{{ single.strIngredient2 }}</span>
-                    <span>{{ single.strIngredient3 }}</span>
-                    <span>{{ single.strIngredient4 }}</span>
-                    <span>{{ single.strIngredient5 }}</span>
+            <div class="wrap">
+                <div class="ingre" v-for="ingmea in measingre" :key="ingmea">
+                    <div class="hold">
+                        <p id="par">{{ ingmea.ingredient }}</p>
+                        <div id="chis">
+                            <p id="chi">{{ ingmea.measure }}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -68,36 +69,151 @@ export default {
     data() {
         return {
             id: this.$route.params.id,
-            single: {},
+            single: [],
             result: [],
-            instructions: []
+            instructions: [],
+            ingredients: [],
+            measurements: [],
+            measingre: []
         }
     },
+
+    // updated() {
+    //     this.ingre()
+    // },
 
     created() {
         fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + this.id)
             .then((response) => response.json())
             .then((response) => {
-                console.log(response.meals[0]);
+                // console.log(response.meals[0]);
                 this.single = response.meals[0];
                 this.result = response.meals[0].strInstructions.split('.')
                 this.instructions = this.result.filter(Boolean)
-                console.log(this.instructions)
+                // console.log(this.instructions)
+                console.log(this.single)
+
+                Object.entries(this.single).forEach(([key, value]) => {
+                    if (key.startsWith("strIngredient") && value !== null && value.trim() !== "") {
+
+                        this.ingredients.push(value);
+                    }
+
+                    if (key.startsWith("strMeasure") && value !== null && value.trim() !== "") {
+                        this.measurements.push(value)
+                    }
+                })
+                this.ingredients.forEach((ing, index) => {
+                    this.measingre.push({
+                        ingredient: ing,
+                        measure: this.measurements[index],
+                    })
+                })
+                console.log(this.ingredients)
+                console.log(this.measurements)
+                console.log(this.measingre)
             });
-    }
+    },
+
+    // created() {
+    //     this.single.forEach(([key, value]) => {
+    //         if (key.startsWith("strIngredient") && value.trim() !== "") {
+    //             this.ingredients.push(value);
+    //         }
+    //         console.log(this.ingredients)
+    //     })
+    // }
+
+    // methods: {
+    //     ingre() {
+    //         Object.entries(this.single).forEach(([key, value]) => {
+    //             if (key.startsWith("strIngredient") && value.trim() !== "") {
+    //                 this.ingredients.push(value);
+    //             }
+
+    //             if (key.startsWith("strMeasure") && value.trim()) {
+    //                 this.measurements.push(value)
+    //             }
+    //         })
+    //         console.log('ingredients:', this.ingredients)
+    //         console.log('measurements:', this.measurements)
+    //     }
+
+    // }
+
 }
 </script>
 
 <style scoped>
+.hold {
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 8px 16px;
+    gap: 10px;
+    border: 1px solid #919191;
+    border-radius: 30px;
+}
+
+.hold:hover {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 8px 16px;
+    gap: 10px;
+    background: #FF9F59;
+    border: 1px solid #FF9F59;
+    box-shadow: 0px 4px 14px rgba(250, 139, 58, 0.4), inset 1px 2px 2px rgba(255, 255, 255, 0.29);
+    border-radius: 30px;
+    background: #FF9F59;
+}
+
+.wrap {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 18px;
+}
+
+#par {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 100%;
+    text-transform: capitalize;
+    color: #919191;
+}
+
+#chis {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 2px 8px;
+    gap: 10px;
+    background: #AAAAAA;
+    border-radius: 30px;
+}
+
+#chi {
+    height: 13px;
+    font-family: 'IBM Plex Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 105%;
+    text-transform: capitalize;
+    color: #FFFFFF;
+}
+
 .ingredients {
-    /* width: 590px; */
-    height: 219px;
+    width: 590px;
+    /* height: 219px; */
     background: rgba(255, 255, 255, 0.6);
     box-shadow: 0px 24px 64px -16px rgba(0, 0, 0, 0.1), inset 0px 4px 2px rgba(255, 255, 255, 0.65);
     backdrop-filter: blur(24px);
     border-radius: 10px;
-    padding: 28px 32px;
-    padding-right: 0;
+    padding: 28px 100px 28px 32px;
+    /* padding-right: 0; */
     margin: 122px 425px 72px;
 }
 
@@ -140,6 +256,8 @@ span {
     border-radius: 30px;
     padding: 11px 16px;
 }
+
+
 
 /* .ingre {
     width: 75px;
@@ -189,7 +307,7 @@ li {
     border-radius: 8px;
 }
 
-p {
+.tabs {
     width: 56px;
     height: 10px;
     font-style: normal;
@@ -203,6 +321,7 @@ p {
 
 .side {
     margin-left: 622px;
+    margin-top: 33px;
 }
 
 .chiq {
